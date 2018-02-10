ef06dd910cc7d827cd9eca490029d8c345e3ce04
Dennis Córdoba López <dcordoba@segurosdelmagisterio.com>
2018-02-09 22:53:08 -0600

    Initial web api

---
+++ API/Components/Exceptions/ExceptionManager.cs
@@ -0,0 +1,108 @@
+﻿using DataAcess.Crud;
+using Entities_POJO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exceptions
+{
+    public class ExceptionManager
+    {
+    
+        public string PATH = @"C:\_temp\logs\";
+
+        private static ExceptionManager instance;
+
+        private static Dictionary<int, AppMessage> messages = new Dictionary<int, AppMessage>();
+
+        private ExceptionManager()
+        {
+            LoadMessages();
+        }
+
+        public static ExceptionManager GetInstance()
+        {
+            if (instance == null)
+                instance = new ExceptionManager();
+
+            return instance;
+        }
+
+        public void Process(Exception ex)
+        {
+
+            var bex = new BussinessException();
+
+            if (ex.GetType() == typeof(BussinessException))
+            {
+                bex = (BussinessException)ex;
+            }
+            else
+            {
+                bex = new BussinessException(0, ex);
+            }
+
+            ProcessBussinesException(bex);
+
+        }
+
+        private void ProcessBussinesException(BussinessException bex)
+        {
+            var today = DateTime.Now.ToString("YYYYMMdd");
+            var logName = PATH + today  + "_" + "log.txt";
+
+            var message = bex.Message + "\n" + bex.StackTrace + "\n";
+
+            if (bex.InnerException!=null)
+                message += bex.InnerException.Message + "\n" + bex.InnerException.StackTrace;
+
+            using (StreamWriter w = File.AppendText(logName))
+            {
+                Log(bex.Message, w);
+            }
+
+            bex.AppMessage = GetMessage(bex);
+
+            throw bex;
+    
+        }
+
+        public AppMessage GetMessage(BussinessException bex)
+        {
+
+            var appMessage = new AppMessage();
+            appMessage.Message = "Message not found!";
+
+            if (messages.ContainsKey(bex.ExceptionId))
+                appMessage = messages[bex.ExceptionId];
+
+            return appMessage;
+
+        }
+
+        private void LoadMessages()
+        {
+
+            var crudMessages = new AppMessagesCrudFactory();
+
+            var lstMessages = crudMessages.RetrieveAll<AppMessage>();
+
+            foreach(var appMessage in lstMessages)
+            {
+                messages.Add(appMessage.Id, appMessage);
+            }  
+
+        }
+
+        private void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString());
+            w.WriteLine("  :");
+            w.WriteLine("  :{0}", logMessage);
+            w.WriteLine("-------------------------------");
+        }
+
+    }
+}
---
+++ API/Components/WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using CoreAPI;
+using Entities_POJO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        
+        // GET api/values
+        public IHttpActionResult Get()
+        {
+            var mng = new CustomerManager();
+            var lstCustomers = mng.RetrieveAll();   
+            return Ok(lstCustomers);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
