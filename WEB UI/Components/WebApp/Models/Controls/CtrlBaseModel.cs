496fa397f708f890e0824fa9f5a70669c50600be
Dennis Córdoba López <dcordoba@segurosdelmagisterio.com>
2018-03-07 20:18:20 -0600

    WEB PROJECT WITH CONTROL TABLE

---
+++ WEB UI/Components/WebApp/Models/Controls/CtrlBaseModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models.Controls
+{
+    public class CtrlBaseModel
+    {
+
+        public string Id { get; set; }
+
+
+        private string ReadFileText()
+        {
+            string path = @"C:\Users\dcordoba.SSVMN\CloudStation\Cenfotec\Proyecto 2\repo\cenfo_proyecto_2\WEB UI\Components\WebApp\Models\Controls\";
+            string fileName = this.GetType().Name + ".html";
+
+            path = path + fileName;
+                
+            string text = System.IO.File.ReadAllText(path);
+
+            return text;
+        }
+
+        public string GetHtml()
+        {
+            var html = ReadFileText();
+
+            foreach (var prop in this.GetType().GetProperties())
+            {
+                var value = prop.GetValue(this, null).ToString();
+
+                var tag = string.Format("-#{0}-", prop.Name);
+                html = html.Replace(tag, value);
+            }
+            return html;
+        }
+    }
+}
---
+++ WEB UI/Components/WebApp/Models/Helpers/ControlExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models.Controls;
+
+namespace WebApp.Helpers
+{
+    public static class ControlExtensions
+    {
+        public static HtmlString CtrlTable(this HtmlHelper html, string id, string title, string columnsTitle, string ColumnsDataName)
+        {
+            var ctrl = new CtrlTableModel
+            {
+                Id = id,
+                Title = title,
+                Columns = columnsTitle,
+                ColumnsDataName = ColumnsDataName  
+            };
+
+            return new HtmlString(ctrl.GetHtml());
+        }
+
+    }
+}
